6157e49b0d00537cf852ce7f4f5c04a213884824
Edwin Mokoena <mamphane@bbd.co.za>
2024-03-08 16:54:17 +0200

    logic to create rds instance

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -1,10 +1,12 @@
 """Pulumi AWS resource provisioning"""
 import pulumi
-from pulumi_aws import ec2, get_availability_zones
+from pulumi_aws import ec2, get_availability_zones, rds
 from helper_functions import label, ingress, everyone_cidr_blocks, egress
 
-size = "t2.micro"
+ec2_instance_type = "t2.micro"
+rds_instance_type = "db.t2.micro"
 all_zones = get_availability_zones()
+config = pulumi.Config()
 
 # Setup VPC
 vpc = ec2.Vpc(label("vpc"), cidr_block="10.0.0.0/16")
@@ -23,7 +25,8 @@ route_table = ec2.RouteTable(
         )
     ],
 )
-# Setup Subnets
+
+# Setup EC2 Subnets
 # limiting to 2 zones for speed and to meet minimal requirements.
 zone_names = [all_zones.names[0], all_zones.names[1]]
 subnets = []
@@ -45,11 +48,17 @@ for zone in zone_names:
     subnets.append(subnet)
     route_table_associations.append(route_table_association)
 
+# Setup RDS subnet group
+rds_subnet_group = rds.SubnetGroup(
+    label("subnet_group"), subnet_ids=[subnet.id for subnet in subnets]
+)
+
 
 # Setup EC2 Security Group
 ec2_security_group = ec2.SecurityGroup(
     label("ec2_security_group"), vpc_id=vpc.id, ingress=ingress(ports=[80, 22, 443])
 )
+
 
 # Setup RDS Security Group
 rds_security_group = ec2.SecurityGroup(
@@ -58,6 +67,24 @@ rds_security_group = ec2.SecurityGroup(
     ingress=ingress(ports=[3306]),
     egress=egress,
 )
+
+# Setup RDS Instance
+rds_instance = rds.Instance(
+    label("rds_instance"),
+    name="mr_bean_store_db",
+    allocated_storage=20,
+    engine="postgres",
+    engine_version="16.2",
+    instance_class=rds_instance_type,
+    storage_type="gp2",
+    db_subnet_group_name=rds_subnet_group.id,
+    username=config.get("db_user"),
+    password=config.get("db_password"),
+    vpc_security_group_ids=[rds_security_group.id],
+    skip_final_snapshot=True,
+    publicly_accessible=False,
+)
+
 
 # Get Amazon Machine Image (Amazon Linux)
 machine_image = ec2.get_ami(
@@ -73,7 +100,7 @@ machine_image = ec2.get_ami(
 
 ec2_instance = ec2.Instance(
     label("api_server"),
-    instance_type=size,
+    instance_type=ec2_instance_type,
     vpc_security_group_ids=[ec2_security_group.id],
     ami=machine_image.id,
 )
@@ -89,3 +116,5 @@ pulumi.export("ec2SecurityGroupId", ec2_security_group.id)
 # pulumi.export("rdsSecurityGroupId", rds_security_group.id)
 pulumi.export("publicIp", ec2_instance.public_ip)
 pulumi.export("publicDns", ec2_instance.public_dns)
+pulumi.export("rdsEndpoint", rds_instance.endpoint)
+pulumi.export("rdsPort", rds_instance.port)
