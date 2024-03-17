49d80895013116cf3af693859695f7994dd6d278
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-03-17 10:15:59 +0200

    editted application.properties

--- server/src/main/java/com/example/devBean/repository/OrderItemRepository.java
+++ server/src/main/java/com/example/devBean/repository/OrderItemRepository.java
@@ -1,9 +1,6 @@
 package com.example.devBean.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.devBean.model.Order;
 import com.example.devBean.model.OrderItem;
 import java.util.List;
